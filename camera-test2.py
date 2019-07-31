ebec7c4bb7028b4b8aae898eb395c095cc8dffa9
michaelmathew <michael.mathew@stonybrook.edu>
2019-07-31 15:48:40 -0400

    Obtained better understanding on using ctrl on image frame for each text box

--- camera-test2.py
+++ camera-test2.py
@@ -215,6 +215,109 @@ def decode_predictions(scores, geometry):
 
 
 
+def extract_text(orig,image):
+    (origH, origW) = image.shape[:2]
+
+    # set the new width and height and then determine the ratio in change
+    # for both the width and height
+    (newW, newH) = (args["width"], args["height"])
+    rW = origW / float(newW)
+    rH = origH / float(newH)
+
+    # resize the image and grab the new image dimensions
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
+
+    # define the two output layer names for the EAST detector model that
+	# we are interested -- the first is the output probabilities and the
+    # second can be used to derive the bounding box coordinates of text
+    layerNames = [
+    "feature_fusion/Conv_7/Sigmoid",
+    "feature_fusion/concat_3"]
+
+    # load the pre-trained EAST text detector
+    print("[INFO] loading EAST text detector...")
+
+    net = cv2.dnn.readNet(args["east"])
+
+    # construct a blob from the image and then perform a forward pass of
+    # the model to obtain the two output layer set
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+
+    # decode the predictions, then  apply non-maxima suppression to
+    # suppress weak, overlapping bounding boxes
+    (rects, confidences) = decode_predictions(scores, geometry)
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+    # initialize the list of results
+    results = []
+
+    # loop over the bounding boxes
+    for (startX, startY, endX, endY) in boxes:
+	    # scale the bounding box coordinates based on the respective
+	    # ratios
+	    startX = int(startX * rW)
+	    startY = int(startY * rH)
+	    endX = int(endX * rW)
+	    endY = int(endY * rH)
+
+	    # in order to obtain a better OCR of the text we can potentially
+	    # apply a bit of padding surrounding the bounding box -- here we
+	    # are computing the deltas in both the x and y directions
+	    dX = int((endX - startX) * args["padding"])
+	    dY = int((endY - startY) * args["padding"])
+
+	    # apply padding to each side of the bounding box, respectively
+	    startX = max(0, startX - dX)
+	    startY = max(0, startY - dY)
+	    endX = min(origW, endX + (dX * 2))
+	    endY = min(origH, endY + (dY * 2))
+
+	    # extract the actual padded ROI
+	    roi = orig[startY:endY, startX:endX]
+
+	    # in order to apply Tesseract v4 to OCR text we must supply
+	    # (1) a language, (2) an OEM flag of 4, indicating that the we
+	    # wish to use the LSTM neural net model for OCR, and finally
+	    # (3) an OEM value, in this case, 7 which implies that we are
+	    # treating the ROI as a single line of text
+	    config = ("-l eng --oem 1 --psm 7")
+	    text = pytesseract.image_to_string(roi, config=config)
+
+	    # add the bounding box coordinates and OCR'd text to the list
+	    # of results
+	    results.append(((startX, startY, endX, endY), text))
+
+    # sort the results bounding box coordinates from top to bottom
+    results = sorted(results, key=lambda r: r[0][1])
+
+    # loop over the results
+    for ((startX, startY, endX, endY), text) in results:
+	    # display the text OCR'd by Tesseract
+	    print("{}\n".format(text))
+
+	    # strip out non-ASCII text so we can draw the text on the image
+	    # using OpenCV, then draw the text and a bounding box surrounding
+	    # the text region of the input image
+	    text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+	    print(text)
+        f= open("guru99.txt","w+")######
+	    output = orig.copy()
+	    cv2.rectangle(output, (startX, startY), (endX, endY),
+                   (0, 0, 255), 2)
+	    cv2.putText(output, text, (startX, startY - 20),
+                 cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+	    # show the output image
+	    cv2.imshow("Text Detection", output)
+	    cv2.waitKey(0)
+
+
+
+
+
 
 
 
@@ -287,7 +390,7 @@ outputLayers.append("feature_fusion/concat_3")
 
 
 #######
-while cv2.waitKey(1) < 0:
+while cv2.waitKey(1) < 0:#continues to scan until user presses a key in terminal 
     #loop always while running to check for text or not 
     # Read frame
     hasFrame, frame = cap.read()
@@ -329,112 +432,12 @@ while cv2.waitKey(1) < 0:
         for j in range(4):
             p1 = (vertices[j][0], vertices[j][1])
             p2 = (vertices[(j + 1) % 4][0], vertices[(j + 1) % 4][1])
-            cv2.line(frame, p1, p2, (0, 255, 0), 2, cv2.LINE_AA)
+            #cv2.line(frame, p1, p2, (0, 255, 0), 2, cv2.LINE_AA)
+        #print("RECTABGLENSSSM")#Every green ractangle
     cv2.imshow(kWinName,frame)
     image=frame
     orig = image.copy()
-
-	
-
-
-
-
-
-    (origH, origW) = image.shape[:2]
-
-    # set the new width and height and then determine the ratio in change
-    # for both the width and height
-    (newW, newH) = (args["width"], args["height"])
-    rW = origW / float(newW)
-    rH = origH / float(newH)
-
-    # resize the image and grab the new image dimensions
-    image = cv2.resize(image, (newW, newH))
-    (H, W) = image.shape[:2]
-
-    # define the two output layer names for the EAST detector model that
-	# we are interested -- the first is the output probabilities and the
-    # second can be used to derive the bounding box coordinates of text
-    layerNames = [
-    "feature_fusion/Conv_7/Sigmoid",
-    "feature_fusion/concat_3"]
-
-    # load the pre-trained EAST text detector
-    print("[INFO] loading EAST text detector...")
-    net = cv2.dnn.readNet(args["east"])
-
-    # construct a blob from the image and then perform a forward pass of
-    # the model to obtain the two output layer set
-    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB=True, crop=False)
-    net.setInput(blob)
-    (scores, geometry) = net.forward(layerNames)
-
-    # decode the predictions, then  apply non-maxima suppression to
-    # suppress weak, overlapping bounding boxes
-    (rects, confidences) = decode_predictions(scores, geometry)
-    boxes = non_max_suppression(np.array(rects), probs=confidences)
-
-    # initialize the list of results
-    results = []
-
-    # loop over the bounding boxes
-    for (startX, startY, endX, endY) in boxes:
-	    # scale the bounding box coordinates based on the respective
-	    # ratios
-	    startX = int(startX * rW)
-	    startY = int(startY * rH)
-	    endX = int(endX * rW)
-	    endY = int(endY * rH)
-
-	    # in order to obtain a better OCR of the text we can potentially
-	    # apply a bit of padding surrounding the bounding box -- here we
-	    # are computing the deltas in both the x and y directions
-	    dX = int((endX - startX) * args["padding"])
-	    dY = int((endY - startY) * args["padding"])
-
-	    # apply padding to each side of the bounding box, respectively
-	    startX = max(0, startX - dX)
-	    startY = max(0, startY - dY)
-	    endX = min(origW, endX + (dX * 2))
-	    endY = min(origH, endY + (dY * 2))
-
-	    # extract the actual padded ROI
-	    roi = orig[startY:endY, startX:endX]
-
-	    # in order to apply Tesseract v4 to OCR text we must supply
-	    # (1) a language, (2) an OEM flag of 4, indicating that the we
-	    # wish to use the LSTM neural net model for OCR, and finally
-	    # (3) an OEM value, in this case, 7 which implies that we are
-	    # treating the ROI as a single line of text
-	    config = ("-l eng --oem 1 --psm 7")
-	    text = pytesseract.image_to_string(roi, config=config)
-
-	    # add the bounding box coordinates and OCR'd text to the list
-	    # of results
-	    results.append(((startX, startY, endX, endY), text))
-
-    # sort the results bounding box coordinates from top to bottom
-    results = sorted(results, key=lambda r: r[0][1])
-
-    # loop over the results
-    for ((startX, startY, endX, endY), text) in results:
-	    # display the text OCR'd by Tesseract
-	    print("{}\n".format(text))
-
-	    # strip out non-ASCII text so we can draw the text on the image
-	    # using OpenCV, then draw the text and a bounding box surrounding
-	    # the text region of the input image
-	    text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
-	    print(text)
-	    output = orig.copy()
-	    cv2.rectangle(output, (startX, startY), (endX, endY),
-                   (0, 0, 255), 2)
-	    cv2.putText(output, text, (startX, startY - 20),
-                 cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
-
-	    # show the output image
-	    cv2.imshow("Text Detection", output)
-	    cv2.waitKey(0)
+    extract_text(orig,image)######press ctrl on image to get all text!!!!!
 
 
 
